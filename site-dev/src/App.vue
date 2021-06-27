5df6b3cd548cf2e1472f036ee735f7e3a5e6bc59
Leonardo dos Santos <Leonardo-Sants@github.com>
2021-06-27 11:33:26 -0300

    Criando api

--- site-dev/src/App.vue
+++ site-dev/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <Header></Header>
     <Home></Home>
+    <Footer></Footer>
   </div>
 </template>
 
@@ -10,13 +11,15 @@ import './styles/global.css';
 
 import Header from './components/HeaderComponent'
 import Home from './pages/home.vue'
+import Footer from './components/footerComponent.vue'
 
 
 export default {
   name: 'App',
   components: {
     Header,
-    Home
+    Home,
+    Footer
   }
 }
 </script>
---
+++ site-dev/src/components/footerComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer>
+        <div></div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+    footer {
+        background-color: var(--color-background-nav);
+    }
+</style>
