5b9825d4dd58b1b41c448adc9331feccada73ee2
Leonardo dos Santos <Leonardo-Sants@github.com>
2021-06-27 11:22:50 -0300

    Criando Footer

--- site-dev/src/pages/home.vue
+++ site-dev/src/pages/home.vue
@@ -1,6 +1,15 @@
 <template>
     <main>
 
+        <div class="text-top">Vídeos novos toda <b>quinta </b>às <b>20h</b></div>
+
+        <a href="https://youtube.com/wdevoficial">
+        <img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/subscribe.svg" 
+        alt="Inscreva-se" id="subscribe">
+        </a>
+
+        <div class="text-bottom">youtube.com/wdevoficial</div>
+
     </main>
 </template>
 
@@ -11,5 +20,29 @@ export default {
 </script>
 
 <style scoped>
+    main {
+        background-color: var(--color-background-home);
+        justify-content: center;
+        align-items: center;
+    }
 
+    .text-top {
+        color:  var(--color-text-light);
+        margin-bottom: 20px;
+    }
+
+    #subscribe {
+        width: 340px;
+    }    
+
+    @media (min-width: 700px){
+        #subscribe {
+            width: 400px;
+    }
+    }
+
+    .text-bottom {
+        color: var(--color-text-light);
+        margin-top: 20px;
+    }
 </style>
